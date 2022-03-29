4c71ecaf5858e12c290b1427613a5489ad05ddb0
Eric <erwm48@gmail.com>
2022-03-29 15:45:44 +0100

    Got a very basic AI working

--- Game Play Programming Task 1/Assets/MyStuff/Scripts/Slime.cs
+++ Game Play Programming Task 1/Assets/MyStuff/Scripts/Slime.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    public bool spotted = false;
+    public GameObject player;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (spotted == true)
+        {
+            Vector3 targetDirection = player.transform.position - transform.position;
+            float singleStep = speed * Time.deltaTime;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            Debug.Log("Rotating Slime");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            spotted = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        spotted = false;
     }
 }
