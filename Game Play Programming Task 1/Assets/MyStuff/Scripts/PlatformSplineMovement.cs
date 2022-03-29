079bb49095a412a44419f73cc3732aa4b2ce1822
Eric <erwm48@gmail.com>
2022-03-29 13:29:03 +0100

    D5 and D6 -Submission

--- Game Play Programming Task 1/Assets/MyStuff/Scripts/PlatformSplineMovement.cs
+++ Game Play Programming Task 1/Assets/MyStuff/Scripts/PlatformSplineMovement.cs
@@ -53,10 +53,10 @@ public class PlatformSplineMovement : MonoBehaviour
 
     private Vector3 QuadraticLerp(Vector3 a, Vector3 b, Vector3 c, float t)
     {
-        Vector3 ab = Vector3.Slerp(a, b, t);
-        Vector3 bc = Vector3.Slerp(b, c, t);
+        Vector3 ab = Vector3.Lerp(a, b, t);
+        Vector3 bc = Vector3.Lerp(b, c, t);
 
-        return Vector3.Slerp(ab, bc, interpolateAmount * step);
+        return Vector3.Lerp(ab, bc, interpolateAmount * step);
     }
 
     private Vector3 CubicLerp(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
@@ -64,6 +64,6 @@ public class PlatformSplineMovement : MonoBehaviour
         Vector3 ab_bc = QuadraticLerp(a, b, c, t);
         Vector3 bc_cd = QuadraticLerp(b, c, d, t);
 
-        return Vector3.Slerp(ab_bc, bc_cd, interpolateAmount * step);
+        return Vector3.Lerp(ab_bc, bc_cd, interpolateAmount * step);
     }
 }
